24a60df5a82bfdfac966e954a1fb9481f4af98c1
Gavin Lynch <gavinlynch04@gmail.com>
2023-11-01 08:20:21 -0700

    Working on implementing atlas

--- packages/backend/backend.js
+++ packages/backend/backend.js
@@ -1,12 +1,52 @@
 import express from "express";
 import cors from "cors";
+import mongoose from 'mongoose';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import bcrypt from 'bcrypt';
 import { readFile } from 'fs/promises';
+
+const username = encodeURIComponent("gavinlynch04");
+const password = encodeURIComponent("HLkQPQ8sHAzRhbog");
+const uri = "mongodb+srv://${username}:${password}@receipt.xjnd3x0.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 const port = 8000;
 
+/*mongoose.connect('mongodb://localhost:27017/receiptDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});*/
+
 app.use(cors());
 app.use(express.json());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("admin");
+        const ratings = database.collection("");
+        const cursor = ratings.find();
+        await cursor.forEach(doc => console.dir(doc));
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+
+const User = mongoose.model('User', UserSchema);
 
 app.get("/receipt", async (req, res) => {
     // Replace 'receipt.json' with the actual path to your JSON file
