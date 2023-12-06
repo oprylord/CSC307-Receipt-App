589130400e986b125fe82edbfa1d61f152648f4e
oprylord <91160121+oprylord@users.noreply.github.com>
2023-12-06 08:48:43 -0800

    Update backend.js

--- packages/backend/backend.js
+++ packages/backend/backend.js
@@ -15,14 +15,8 @@ const app = express();
 const port = 8000;
 const saltRounds = 10;
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 dotenv.config();
