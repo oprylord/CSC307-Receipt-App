99ed1431932b021bfbaf90cfec0ae37a7bc64678
oprylord <91160121+oprylord@users.noreply.github.com>
2023-12-06 08:38:01 -0800

    cors?

--- packages/backend/backend.js
+++ packages/backend/backend.js
@@ -15,7 +15,14 @@ const app = express();
 const port = 8000;
 const saltRounds = 10;
 
-app.use(cors());
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 dotenv.config();
